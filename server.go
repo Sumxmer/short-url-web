7b31f342f21499a059a8f58fe1ca6792bba6f7a0
Sumxmer <napat.kuntawat.01@gmail.com>
2026-03-31 11:17:10 +0700

    Railway deploy ready

--- server.go
+++ server.go
@@ -5,41 +5,44 @@ import (
 	"html/template"
 	"math/rand"
 	"net/http"
-	
+	"os"
 	"sync"
-	
 	"time"
 )
 
 var (
 	urlmap = make(map[string]string)
-	mu sync.Mutex
+	mu     sync.Mutex
 )
 
-func main(){
+func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	http.HandleFunc("/",homeHandler)
-	http.HandleFunc("/shorten",shortenHandler)
-	http.HandleFunc("/r/",redirectHandler)
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/shorten", shortenHandler)
+	http.HandleFunc("/r/", redirectHandler)
 	fmt.Println("server run http://localhost:8080")
-	http.ListenAndServe(":8080",nil)
+	http.ListenAndServe(":8080", nil)
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.ListenAndServe(":"+port, nil)
 
-	
 }
 
-func homeHandler(w http.ResponseWriter,r *http.Request){
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("index.html"))
 
 	mu.Lock()
 	defer mu.Unlock()
-	tmpl.Execute(w,urlmap)
+	tmpl.Execute(w, urlmap)
 }
 
-func shortenHandler(w http.ResponseWriter,r *http.Request){
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		http.Redirect(w,r,"/",http.StatusSeeOther)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 
 	r.ParseForm()
@@ -50,30 +53,30 @@ func shortenHandler(w http.ResponseWriter,r *http.Request){
 	urlmap[code] = originalURL
 	mu.Unlock()
 
-	http.Redirect(w,r,"/",http.StatusSeeOther)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
-func redirectHandler(w http.ResponseWriter,r *http.Request){
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
 
 	code := r.URL.Path[len("/r/"):]
 
 	mu.Lock()
-	url,ok := urlmap[code]
+	url, ok := urlmap[code]
 	mu.Unlock()
 
 	if ok {
-		http.Redirect(w,r,url,http.StatusSeeOther)
-	}else{
-		http.NotFound(w,r)
+		http.Redirect(w, r, url, http.StatusSeeOther)
+	} else {
+		http.NotFound(w, r)
 	}
 }
 
-func randomString(n int ) string{
+func randomString(n int) string {
 	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	b := make([]rune,n)
+	b := make([]rune, n)
 
 	for i := range b {
 		b[i] = letters[rand.Intn(len(letters))]
-		
+
 	}
 	return string(b)
 }
